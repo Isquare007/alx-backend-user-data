0dd60a3db524bf04fe9da2964b3af75fb961a223
Isquare007 <akibrahiim2019@gmail.com>
2023-05-25 06:23:05 +0100

    filterd_datum

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -29,27 +29,6 @@ def filter_datum(fields: List[str], redaction: str,
                          f'{field}={redaction}{separator}', message)
     return message
 
-
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        message = super().format(record)
-        message = filter_datum(
-            self.fields, self.REDACTION, message, self.SEPARATOR)
-        record.msg = message
-        return super().format(record)
-
-
 def get_logger() -> logging.Logger:
     """set up a logger with formatter, streamhandler and level"""
     logger = logging.getLogger("user_data")
@@ -75,9 +54,29 @@ def get_db():
 
     return db_connect
 
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = super().format(record)
+        message = filter_datum(
+            self.fields, self.REDACTION, message, self.SEPARATOR)
+        record.msg = message
+        return super().format(record)
+
 
 def main():
-    """main function; fetches data from a db and hashes the PII"""
+    """main function; fetches data
+    from a db and hashes the PII"""
     db_connector = get_db()
     cursor = db_connector.cursor()
     cursor.execute("SELECT * FROM users;")
