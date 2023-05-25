657a40cc5f8525165fbf5ed55c359c305d4845a9
Isquare007 <akibrahiim2019@gmail.com>
2023-05-25 06:51:53 +0100

    executable

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,12 +7,22 @@ from os import environ
 import re
 from typing import List
 
-PII_FIELDS = ['name', 'email', 'phone', 'ssn', 'password']
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """filters the person data from a message
+
+    Args:
+        fields (list): a list of strings representing
+        all fields to obfuscate
+        redaction (str): a string representing by what
+        the field will be obfuscated
+        message (str): a string representing the log line
+        separator (str): a string representing by whic
+        character is separating all
+        fields in the log line (message)
     """
     for field in fields:
         message = re.sub(f'{field}=.*?{separator}',
@@ -24,6 +34,7 @@ def get_logger() -> logging.Logger:
     """set up a logger with formatter, streamhandler and level"""
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
+    logger.propagate = False
 
     formatter = RedactingFormatter(list(PII_FIELDS))
     stream_handler = logging.StreamHandler()
@@ -33,7 +44,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """uses my-sql connector to connect to a db"""
     username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
     password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
