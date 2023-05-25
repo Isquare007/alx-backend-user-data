e8322a97f86cc87771c188b0420f104c1385ef3d
Isquare007 <akibrahiim2019@gmail.com>
2023-05-25 06:47:36 +0100

    doc string

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,16 +13,6 @@ PII_FIELDS = ['name', 'email', 'phone', 'ssn', 'password']
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """filters the person data from a message
-
-    Args:
-        fields (list): a list of strings representing
-        all fields to obfuscate
-        redaction (str): a string representing by what
-        the field will be obfuscated
-        message (str): a string representing the log line
-        separator (str): a string representing by whic
-        character is separating all
-        fields in the log line (message)
     """
     for field in fields:
         message = re.sub(f'{field}=.*?{separator}',
