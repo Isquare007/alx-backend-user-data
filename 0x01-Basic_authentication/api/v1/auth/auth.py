54a7ec22c969a41ee91ea69783b53f7ba110056b
Isquare007 <akibrahiim2019@gmail.com>
2023-05-29 23:21:17 +0100

    Require auth with stars

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -21,6 +21,10 @@ class Auth:
         for excluded_path in excluded_paths:
             if path == excluded_path.rstrip('/'):
                 return False
+            if excluded_path.endswith('*') and path.startswith(excluded_path.rstrip('*')):
+                return False
+            elif path == excluded_path:
+                return False
 
         return True
 
