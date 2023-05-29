286fbc8670753a141c6501d91001296b698f6788
Isquare007 <akibrahiim2019@gmail.com>
2023-05-29 20:23:05 +0100

    tuple

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -37,7 +37,8 @@ class BasicAuth(Auth):
             return None
 
     def extract_user_credentials(
-            self, decoded_base64_authorization_header: str) -> tuple((str, str)):
+        self, decoded_base64_authorization_header:
+            str) -> tuple((str, str)):
         """extracts user email and password"""
         if decoded_base64_authorization_header is None:
             return None, None
