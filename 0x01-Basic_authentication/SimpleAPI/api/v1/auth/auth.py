809a3d7b7e1905ba996db666821ef8d47a7a792e
Isquare007 <akibrahiim2019@gmail.com>
2023-05-29 14:30:07 +0100

    routes that don't need authnetication

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -10,10 +10,22 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require auth method"""
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        
+        if path.endswith('/'):
+            path = path[:-1]
+        
+        for excluded_path in excluded_paths:
+            if path == excluded_path.rstrip('/'):
+                return False
+        
+        return True
 
     def authorization_header(self, request=None) -> str:
-        """Uthorization header"""
+        """Unauthorization header"""
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
