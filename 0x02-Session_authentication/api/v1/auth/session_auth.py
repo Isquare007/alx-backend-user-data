213c09b5ccdbe43101d0add8c5a56f910974e5e1
Isquare007 <akibrahiim2019@gmail.com>
2023-06-01 21:06:34 +0100

    task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,22 +1,32 @@
 #!/usr/bin/env python3
-"""Session authentication"""
-from api.v1.auth.auth import Auth
+'''Implement Session Authentication'''
+from .auth import Auth
+from uuid import uuid4
 from models.user import User
-import uuid
 
 
 class SessionAuth(Auth):
-    """SessionAuth"""
+    '''defines methods and attributes for implementing
+    session authentication'''
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """creates user session"""
-        if user_id is None and not isinstance(user_id, str):
+        '''create a session ID for a user_id
+
+        Args:
+            user_id (str, optional): User id. Defaults to None.
+
+        Returns:
+            str: a uuid for user
+        '''
+        if user_id is None or not isinstance(user_id, str):
             return None
 
-        s_id = str(uuid.uuid4())
-        self.user_id_by_session_id[s_id] = user_id
-        return s_id
+        session_id = str(uuid4())
+
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
 
     # def user_id_for_session_id(self, session_id: str = None) -> str:
     #     """returns the user id based on the user session id"""
