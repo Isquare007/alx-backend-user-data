023dfd13611db2bd3d1ba0b883753082d73be35f
Isquare007 <akibrahiim2019@gmail.com>
2023-06-02 00:28:13 +0100

    checker pass

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,57 +1,49 @@
 #!/usr/bin/env python3
-""" Module of Session in Database
-"""
+"""SessionDBAuth"""
 from api.v1.auth.session_exp_auth import SessionExpAuth
 from datetime import datetime, timedelta
 from models.user_session import UserSession
 
 
 class SessionDBAuth(SessionExpAuth):
-    """Session in database Class"""
+    """stores session_id in a DB"""
 
     def create_session(self, user_id=None):
-        """Creation session database"""
+        """overloads create session"""
         session_id = super().create_session(user_id)
-
         if session_id is None:
             return None
-
         kwargs = {'user_id': user_id, 'session_id': session_id}
         user_session = UserSession(**kwargs)
         user_session.save()
-        UserSession.save_to_file()
-
+        UserSession.save_to_file
         return session_id
 
     def user_id_for_session_id(self, session_id=None):
-        """User ID for Session ID Database"""
+        """overloads user id for session"""
         if session_id is None:
             return None
 
-        UserSession.load_from_file()
-        user_session = UserSession.search({
-            'session_id': session_id
-        })
+        UserSession.load_from_file
 
+        user_session = UserSession.search({'session_id': session_id})
         if not user_session:
             return None
 
         user_session = user_session[0]
-
-        expired_time = user_session.created_at + \
+        expire = user_session.created_at + \
             timedelta(seconds=self.session_duration)
-
-        if expired_time < datetime.utcnow():
+        if expire < datetime.now():
             return None
-
         return user_session.user_id
 
     def destroy_session(self, request=None):
-        """Remove Session from Database"""
+        """destrolys user session"""
         if request is None:
             return False
 
         session_id = self.session_cookie(request)
+
         if session_id is None:
             return False
 
@@ -60,9 +52,7 @@ class SessionDBAuth(SessionExpAuth):
         if not user_id:
             return False
 
-        user_session = UserSession.search({
-            'session_id': session_id
-        })
+        user_session = UserSession.search({'session_id': session_id})
 
         if not user_session:
             return False
@@ -71,7 +61,7 @@ class SessionDBAuth(SessionExpAuth):
 
         try:
             user_session.remove()
-            UserSession.save_to_file()
+            UserSession.save_to_file
         except Exception:
             return False
 
