9d31e9a07724d9b63bbdc36235cc64369b0ad68f
Isquare007 <akibrahiim2019@gmail.com>
2023-06-06 08:31:21 +0100

    update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,8 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-# from sqlalchemy.exc import InvalidRequestError
-# from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 # from typing import TypeVar
 from user import Base, User
 
@@ -38,3 +38,38 @@ class DB:
         self._session.commit()
 
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ Finds user by key word args
+        Return: First row found in the users table as filtered by kwargs
+        """
+        if not kwargs:
+            raise InvalidRequestError
+
+        column = User.__table__.columns.keys()
+
+        for key in kwargs.keys():
+            if key not in column:
+                raise InvalidRequestError
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+
+        if user is None:
+            raise NoResultFound
+
+        return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """updates user data"""
+        try:
+            user = self.find_user_by(id=user_id)
+        except NoResultFound:
+            raise ValueError
+
+        for attr, value in kwargs.items():
+            if not hasattr(user, attr):
+                raise ValueError
+
+            setattr(user, attr, value)
+
+        self._session.commit()
