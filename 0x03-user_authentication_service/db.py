860838439e93147355ec5c93f4006626da672137
Isquare007 <akibrahiim2019@gmail.com>
2023-06-06 08:05:05 +0100

    Find User

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+""" Database for ORM """
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import InvalidRequestError, NoResultFound
+from typing import TypeVar
+from user import Base, User
+
+
+class DB:
+    """ DB Class for Object Reational Mapping """
+
+    def __init__(self):
+        """ Constructor Method """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self):
+        """ Session Getter Method """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """ Adds user to database
+
+        Return: User Object
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ Finds user by key word args
+        Return: First row found in the users table as filtered by kwargs
+        """
+        if not kwargs:
+            raise InvalidRequestError
+
+        column = User.__table__.columns.keys()
+
+        for key in kwargs.keys():
+            if key not in column:
+                raise InvalidRequestError
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+
+        if user is None:
+            raise NoResultFound
+
+        return user
